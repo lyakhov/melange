a78f7a383b364ac6e15be3e9c8cbe55da43ea8c0
Yegor Mazur <yegor.mazur@gmail.com>
2014-01-14 01:59:38 +0400

    Added wrtc-softphone target.

---
+++ src/wrtc-softphone/gui/MainWindow.cpp
@@ -0,0 +1,41 @@
+/* MainWindow.cpp
+ *
+ * Copyright 2014 Yegor Mazur <yegor.mazur@gmail.com>
+ *
+ * This file is part of Melange.
+ *
+ * Melange is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Melange is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Melange. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file was derived from helloworld.c test/example of GStreamer framework.
+ */
+
+#include "MainWindow.h"
+#include <QVBoxLayout>
+
+MainWindow::MainWindow()
+{
+	setFixedSize(300, 200);
+	
+	QVBoxLayout audioDeviceLayout;
+	
+	m_AudioDeviceLabel = new QLabel("Audio Device", this);
+	m_AudioDeviceCombox = new QComboBox(this);
+	
+	audioDeviceLayout.addWidget(m_AudioDeviceLabel);
+	audioDeviceLayout.addWidget(m_AudioDeviceCombox);
+}
+
+MainWindow::~MainWindow()
+{
+}
---
+++ src/wrtc-softphone/gui/main.cpp
@@ -0,0 +1,35 @@
+/* main.cpp
+ *
+ * Copyright 2014 Yegor Mazur <yegor.mazur@gmail.com>
+ *
+ * This file is part of Melange.
+ *
+ * Melange is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Melange is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Melange. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file was derived from helloworld.c test/example of GStreamer framework.
+ */
+
+
+#include "MainWindow.h"
+#include <QApplication>
+
+int main(int argc, char **argv)
+{
+	QApplication app(argc, argv);
+	
+	MainWindow mainwindow;
+	mainwindow.show();
+	app.exec();
+	return 0;
+}
