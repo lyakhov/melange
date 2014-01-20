4bd442c137fc0a620934951455f51491538ca0cc
Alexey Kuzin <amkuzink@gmail.com>
2014-01-20 17:14:35 +0400

    Implement gst_init, gst_element_factory_make, gst_uri_is_valid, gst_filename_to_uri in gst-dbus-stubs.c

--- src/gst-dbus-stubs/gst-dbus-stubs.c
+++ src/gst-dbus-stubs/gst-dbus-stubs.c
@@ -1,6 +1,10 @@
 /* gst-dbus-stubs.c: GStreamer API stubs redirecting calls to gstd, the GStreamer D-Bus Daemon
  *
  * Copyright 2014 Fedor Lyakhov <fedor.lyakhov@gmail.com>
+ * Copyright (C) 1999,2000 Erik Walthinsen <omega@cse.ogi.edu>
+ *                    2000 Wim Taymans <wtay@chello.be>
+ * Copyright (C) 2011 Tim-Philipp MÃ¼ller <tim centricular net>
+ * Copyright 2014 Alexey Kuzin <amkuzink@gmail.com>
  *
  * This file is part of Melange.
  *
@@ -19,6 +23,10 @@
  *
  */
 #include "gst/gst.h"
+#include <gio/gio.h>
+#include <string.h>
+
+static gchar *media_file_path;
 
 void gst_message_parse_error(GstMessage *message, GError **gerror, gchar **debug)
 {
@@ -26,11 +34,48 @@ void gst_message_parse_error(GstMessage *message, GError **gerror, gchar **debug
 
 void gst_init(int *argc, char **argv[])
 {
+	media_file_path = (gchar *)argv[1];
 }
 
 GstElement *gst_element_factory_make(const gchar *factoryname, const gchar *name)
 {
-	return NULL;
+	GDBusProxy *proxy = NULL;
+	GVariant *variant = NULL;
+	GError *error = NULL;
+
+	proxy = g_dbus_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION,
+		G_DBUS_PROXY_FLAGS_NONE,
+		NULL,
+		"com.ridgerun.gstreamer.gstd",
+		"/com/ridgerun/gstreamer/gstd/factory",
+		"com.ridgerun.gstreamer.gstd.FactoryInterface",
+		NULL,
+		&error);
+
+	if(error)
+	{
+		g_printerr("g_dbus_proxy_new_for_bus_sync failed: %s\n", error->message);
+		return NULL;
+	}
+
+	variant = NULL;
+
+	variant = g_dbus_proxy_call_sync(proxy,
+		"Create",
+		g_variant_new("(s)", "playbin"), G_DBUS_CALL_FLAGS_NONE,
+		-1,
+		NULL,
+		&error);
+
+	if (proxy)
+		g_object_unref(proxy);
+	if(error)
+	{
+		g_printerr("g_dbus_proxy_call_sync failed: %s\n", error->message);
+		return NULL;
+	}
+
+	return (GstElement*)variant;
 }
 
 GstBus *gst_element_get_bus(GstElement *element)
@@ -48,12 +93,114 @@ guint gst_bus_add_watch(GstBus *bus, GstBusFunc func, gpointer user_data)
 	return 0;
 }
 
+// The following functions were copied from 'gst/gsturi.c' file of GStreamer
 gboolean gst_uri_is_valid(const gchar *uri)
 {
-	return FALSE;
+	gchar *endptr;
+
+	g_return_val_if_fail (uri != NULL, FALSE);
+
+	gchar *check = (gchar *) uri;
+
+	g_assert (uri != NULL);
+	g_assert (endptr != NULL);
+
+	if (g_ascii_isalpha (*check)) {
+		check++;
+		while (g_ascii_isalnum (*check) || *check == '+'
+			|| *check == '-' || *check == '.')
+			check++;
+	}
+	endptr = check;
+
+	return *endptr == ':';
+}
+
+static gboolean
+file_path_contains_relatives (const gchar * path)
+{
+	return (strstr (path, "/./") != NULL || strstr (path, "/../") != NULL ||
+	strstr (path, G_DIR_SEPARATOR_S "." G_DIR_SEPARATOR_S) != NULL ||
+	strstr (path, G_DIR_SEPARATOR_S ".." G_DIR_SEPARATOR_S) != NULL);
+}
+
+static gchar *
+gst_file_utils_canonicalise_path (const gchar * path)
+{
+	gchar **parts, **p, *clean_path;
+
+	parts = g_strsplit (path, "/", -1);
+
+	p = parts;
+	while (*p != NULL) {
+		if (strcmp (*p, ".") == 0) {
+			/* just move all following parts on top of this, incl. NUL terminator */
+			g_free (*p);
+			g_memmove (p, p + 1, (g_strv_length (p + 1) + 1) * sizeof (gchar *));
+			/* re-check the new current part again in the next iteration */
+			continue;
+		} else if (strcmp (*p, "..") == 0 && p > parts) {
+			/* just move all following parts on top of the previous part, incl.
+			 * NUL terminator */
+			g_free (*(p - 1));
+			g_free (*p);
+			g_memmove (p - 1, p + 1, (g_strv_length (p + 1) + 1) * sizeof (gchar *));
+			/* re-check the new current part again in the next iteration */
+			--p;
+			continue;
+		}
+		++p;
+	}
+	if (*path == '/') {
+		guint num_parts;
+
+		num_parts = g_strv_length (parts) + 1;      /* incl. terminator */
+		parts = g_renew (gchar *, parts, num_parts + 1);
+		g_memmove (parts + 1, parts, num_parts * sizeof (gchar *));
+		parts[0] = g_strdup ("/");
+	}
+
+	clean_path = g_build_filenamev (parts);
+	g_strfreev (parts);
+	return clean_path;
 }
 
 gchar *gst_filename_to_uri(const gchar *filename, GError **error)
 {
-	return NULL;
+	gchar *abs_location = NULL;
+	gchar *uri, *abs_clean;
+
+	g_return_val_if_fail (filename != NULL, NULL);
+	g_return_val_if_fail (error == NULL || *error == NULL, NULL);
+
+	if (g_path_is_absolute (filename)) {
+		if (!file_path_contains_relatives (filename)) {
+			uri = g_filename_to_uri (filename, NULL, error);
+			goto beach;
+		}
+
+		abs_location = g_strdup (filename);
+	} else {
+		gchar *cwd;
+
+		cwd = g_get_current_dir ();
+		abs_location = g_build_filename (cwd, filename, NULL);
+		g_free (cwd);
+
+		if (!file_path_contains_relatives (abs_location)) {
+			uri = g_filename_to_uri (abs_location, NULL, error);
+			goto beach;
+		}
+	}
+
+	/* path is now absolute, but contains '.' or '..' */
+	abs_clean = gst_file_utils_canonicalise_path (abs_location);
+	g_print ("'%s' -> '%s' -> '%s'", filename, abs_location, abs_clean);
+	uri = g_filename_to_uri (abs_clean, NULL, error);
+	g_free (abs_clean);
+
+beach:
+	g_free (abs_location);
+	g_print ("'%s' -> '%s'", filename, uri);
+	return uri;
 }
